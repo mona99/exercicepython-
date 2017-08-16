2cc06e9192e67ac821b51fe37ce23b11db82dc49
mona <mouna.nouira@temelio.com>
2017-08-16 13:38:27 +0000

    program that calculte the speed

--- exercicepython.py
+++ exercicepython.py
@@ -1,16 +1,5 @@
 #-*-coding:utf-8 -*-
-i = 1
-
-while i < 20: # Tant que i est inférieure à 20
-
-    if i % 3 == 0:
-
-        i += 4 # On ajoute 4 à i
-
-        print("On incrémente i de 4. i est maintenant égale à", i)
-
-        continue # On retourne au while sans exécuter les autres lignes
-
-    print("La variable i =", i)
-
-    i += 1 # Dans le cas classique on ajoute juste 1 à i
+temps= 6.892
+distance= 19.7
+vitesse= distance/temps 
+print("vitesse=",  vitesse) 
