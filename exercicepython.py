85d96fc5b8968fb539f88d4b57a9cf3823ea3221
mona <mouna.nouira@temelio.com>
2017-08-17 15:39:23 +0000

    speed calculator

--- exercicepython.py
+++ exercicepython.py
@@ -1,14 +1,17 @@
 #-*-coding:utf-8 -*-
+"""This program will calculate the speed using the time and the distance"""
 def speed():
-    temps= 6.892
-    distance= 19.7
-    return  distance/temps 
+    """This function is used to calculate the speed"""
+    temps = 6.892
+    distance = 19.7
+    return distance/temps
 
-def affiche(vitesse):
-    print("vitesse=",  vitesse)
+def display(vitesse):
+    """Display  is a function used to show the result"""
+    print("vitesse=", vitesse)
 
 def main():
-    stockage=speed()
-    affiche(stockage)
+    """This function is used to execute the program"""
+    stockage = speed()
+    display(stockage)
 main()
-
