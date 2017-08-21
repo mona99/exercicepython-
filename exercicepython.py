1674040100c2e959b2e9cf080588fde3f706b8dd
mona <mouna.nouira@temelio.com>
2017-08-21 08:28:50 +0000

    file modified

--- exercicepython.py
+++ exercicepython.py
@@ -1,14 +1,16 @@
-
 #-*-coding:utf-8 -*-
-
-def speed(distance,temps):
-     return distance/temps
+""" this program is a speed calculator"""
+def speed(distance, temps):
+    """ This function will calulate the speed"""
+    return distance/temps
 def display(vitesse):
-    print("vitesse=", vitesse)
+    """ Display is used to show the result"""
 
+    print("vitesse=", vitesse)
 def main():
+    """local function"""
     distance = float(input("entrer la distance : "))
     temps = float(input("entrer le temps : "))
-    stockage = speed(distance,temps)
+    stockage = speed(distance, temps)
     display(stockage)
 main()
