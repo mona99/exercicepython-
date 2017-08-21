e1cfc7120559cb46af6ee6a6def9ea6aea101090
mona <mouna.nouira@temelio.com>
2017-08-21 07:50:24 +0000

    modified to local fonction

--- exercicepython.py
+++ exercicepython.py
@@ -3,12 +3,12 @@
 
 def speed(distance,temps):
      return distance/temps
-distance = float(input("entrer la distance : "))
-temps = float(input("entrer le temps : "))
 def display(vitesse):
     print("vitesse=", vitesse)
 
 def main():
+    distance = float(input("entrer la distance : "))
+    temps = float(input("entrer le temps : "))
     stockage = speed(distance,temps)
     display(stockage)
 main()
