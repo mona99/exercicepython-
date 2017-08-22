8af727969f3f82c31ceec94fa5ed961e6031fb25
mona <mouna.nouira@temelio.com>
2017-08-22 13:43:41 +0000

    Testing display function

--- test1.py
+++ test1.py
@@ -1,8 +1,6 @@
 #-*-coding:utf-8 -*-
-from unnecessary_math import multiply
+def display():
+    return 3
+def test-function():
+    assert ('vitesse', 3)
 
-def test_number_3_4():
-    assert multiply(3,4) == 12
-
-def test_string_a_3():
-    assert multiply('a',3) == 'aaa'
